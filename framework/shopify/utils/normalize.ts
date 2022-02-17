3e2207ba6f47fec3338ebcf2c68311704291b70b
JoshuaHolloway <jhollow6@asu.edu>
2022-02-16 22:34:01 -0600

    normalize finished

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -44,7 +44,26 @@ const normalizeProductOption = ({
   console.log('NAME ', displayName);
   console.log('VALUES ', values);
 
-  return {};
+  const normalized = {
+    id,
+    displayName,
+    values: values.map((value) => {
+      let output: any = {
+        label: value,
+      };
+
+      if (displayName.match(/colou?r/gi)) {
+        output = {
+          ...output,
+          hexColor: value,
+        };
+      }
+
+      return output;
+    }),
+  };
+
+  return normalized;
 };
 
 // ==============================================
